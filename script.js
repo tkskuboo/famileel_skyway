7ef5e8968057833276073ea22baacc4d451a027d
tkskuboo <39479892+tkskuboo@users.noreply.github.com>
2020-05-01 15:28:11 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const Peer = window.Peer;
+
+(async function main() {
+    const localVideo = document.getElementById('js-local-video');
+    const localId = document.getElementById('js-local-id');
+    const remoteVideo = document.getElementById('js-remote-video');
+    const remoteId = document.getElementById('js-remote-id');
+    const connectedId = document.getElementById('js-connected-id');
+    const callTrigger = document.getElementById('js-call-trigger');
+    const closeTrigger = document.getElementById('js-close-trigger');
+
+    const localStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: { width:{min: 320, max: 320}, height:{min: 240, max: 240}}
+    });
+    localVideo.srcObject = localStream;
+
+    const peer = new Peer({
+        key: 'b2dfe6f3-79d6-4c36-9150-63a754b06cd7',
+        debug: 3,
+    });
+
+    peer.on('open', (id) => {
+        localId.textContent = id;
+    });
+
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        connectedId.textContent = mediaConnection.remoteId;
+
+        mediaConnection.on('stream', stream => {
+            remoteVideo.srcObject = stream;
+        });
+
+        mediaConnection.once('close', () => {
+            remoteVideo.srcObject.getTracks().forEach(track => {
+                track.stop();
+            });
+            remoteVideo.srcObject = null;
+            connectedId.textContent = '...';
+        });
+
+        closeTrigger.addEventListener('click', () => {
+            mediaConnection.close(true);
+        });
+    });
+
+    callTrigger.addEventListener('click', () => {
+        const mediaConnection = peer.call(remoteId.value, localStream);
+        connectedId.textContent = mediaConnection.remoteId;
+
+        mediaConnection.on('stream', stream => {
+            remoteVideo.srcObject = stream;
+        });
+
+        mediaConnection.once('close', () => {
+            remoteVideo.srcObject.getTracks().forEach(track => {
+                track.stop();
+            });
+            remoteVideo.srcObject = null;
+            connectedId.textContent = '...';
+        });
+
+        closeTrigger.addEventListener('click', () => {
+            mediaConnection.close(true);
+        });
+    });
+
+    peer.on('error', console.error);
+})();
